e8087c00a3237f14d3e3877daf4ec528e3154a82
Trương Quang Tường <21522777@gm.uit.edu.vn>
2025-10-07 17:03:35 +0700

    new commit

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://my-portfolio-be-zeta.vercel.app/", // backend URL
+  baseURL: "https://my-portfolio-be-zeta.vercel.app/api/", // backend URL
   withCredentials: true, // cookie
 });
 
